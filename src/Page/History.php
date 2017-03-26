6ba98dc1333a60799426ff7def3619f52fdb57b8
KwangSeob Jeong <lesstif@gmail.com>
2017-03-26 18:45:42 +0900

    impl page

---
+++ src/Page/History.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Lesstif\Confluence\Page;
+
+
+class History
+{
+    /** @var  boolean */
+    public $latest;
+
+    /** @var  array */
+    public $createdBy;
+
+    /** @var string */
+    public $createdDate;
+
+    /** @var  array */
+    public $_links;
+}
